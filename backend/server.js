d305a87d7c8fb96a695d940e90c0148637002b9f
Jafin Jahfar <jafinjahfar@gmail.com>
2022-12-01 17:26:35 +0530

    Backend completed

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
-const colors = require('colors');
-const {errorHandler} = require('./middleware/errorMiddleware')
-const connectDB = require('./config/db')
+const colors = require("colors");
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
 
 connectDB();
 
@@ -12,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/goals", require("./routes/goalRouter"));
+app.use("/api/users", require("./routes/userRouter"));
 
 app.use(errorHandler);
 
